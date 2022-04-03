97168bba5a9514bf05053b2489462df4c83e5dec
Admin <Admin@DAVID>
2022-04-03 18:32:03 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace Testapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChkInclSubdirs = new System.Windows.Forms.CheckBox();
+            this.SelectedFolderText = new System.Windows.Forms.TextBox();
+            this.BtnFolderSelect = new System.Windows.Forms.Button();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.SelectFolderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ChkSsdDisk = new System.Windows.Forms.CheckBox();
+            this.TextBoxResult = new System.Windows.Forms.TextBox();
+            this.LblReult = new System.Windows.Forms.Label();
+            this.StatBar = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusPrg = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatFileCount = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatTime = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripDuplicityFound = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ChkInclSubdirs
+            // 
+            this.ChkInclSubdirs.AutoSize = true;
+            this.ChkInclSubdirs.Location = new System.Drawing.Point(16, 97);
+            this.ChkInclSubdirs.Name = "ChkInclSubdirs";
+            this.ChkInclSubdirs.Size = new System.Drawing.Size(205, 24);
+            this.ChkInclSubdirs.TabIndex = 0;
+            this.ChkInclSubdirs.Text = "Include Sub-directories";
+            this.ChkInclSubdirs.UseVisualStyleBackColor = true;
+            // 
+            // SelectedFolderText
+            // 
+            this.SelectedFolderText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SelectedFolderText.Location = new System.Drawing.Point(16, 60);
+            this.SelectedFolderText.Name = "SelectedFolderText";
+            this.SelectedFolderText.Size = new System.Drawing.Size(1194, 26);
+            this.SelectedFolderText.TabIndex = 1;
+            // 
+            // BtnFolderSelect
+            // 
+            this.BtnFolderSelect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnFolderSelect.Location = new System.Drawing.Point(1238, 60);
+            this.BtnFolderSelect.Name = "BtnFolderSelect";
+            this.BtnFolderSelect.Size = new System.Drawing.Size(124, 31);
+            this.BtnFolderSelect.TabIndex = 2;
+            this.BtnFolderSelect.Text = "Select folder";
+            this.BtnFolderSelect.UseVisualStyleBackColor = true;
+            this.BtnFolderSelect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.BtnStart.Location = new System.Drawing.Point(509, 135);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(232, 89);
+            this.BtnStart.TabIndex = 3;
+            this.BtnStart.Text = "Start finding file duplicities";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1381, 43);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Find file duplicities";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ChkSsdDisk
+            // 
+            this.ChkSsdDisk.AutoSize = true;
+            this.ChkSsdDisk.Location = new System.Drawing.Point(267, 97);
+            this.ChkSsdDisk.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ChkSsdDisk.Name = "ChkSsdDisk";
+            this.ChkSsdDisk.Size = new System.Drawing.Size(139, 24);
+            this.ChkSsdDisk.TabIndex = 5;
+            this.ChkSsdDisk.Text = "SSD disk type";
+            this.ChkSsdDisk.UseVisualStyleBackColor = true;
+            // 
+            // TextBoxResult
+            // 
+            this.TextBoxResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBoxResult.Location = new System.Drawing.Point(13, 270);
+            this.TextBoxResult.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TextBoxResult.Multiline = true;
+            this.TextBoxResult.Name = "TextBoxResult";
+            this.TextBoxResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextBoxResult.Size = new System.Drawing.Size(1343, 527);
+            this.TextBoxResult.TabIndex = 9;
+            // 
+            // LblReult
+            // 
+            this.LblReult.AutoSize = true;
+            this.LblReult.Location = new System.Drawing.Point(12, 245);
+            this.LblReult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LblReult.Name = "LblReult";
+            this.LblReult.Size = new System.Drawing.Size(66, 20);
+            this.LblReult.TabIndex = 7;
+            this.LblReult.Text = "Results";
+            // 
+            // StatBar
+            // 
+            this.StatBar.ImageScalingSize = new System.Drawing.Size(26, 26);
+            this.StatBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusPrg,
+            this.toolStripStatFileCount,
+            this.toolStripStatTime,
+            this.toolStripDuplicityFound});
+            this.StatBar.Location = new System.Drawing.Point(0, 807);
+            this.StatBar.Name = "StatBar";
+            this.StatBar.Padding = new System.Windows.Forms.Padding(2, 0, 21, 0);
+            this.StatBar.Size = new System.Drawing.Size(1381, 28);
+            this.StatBar.TabIndex = 8;
+            this.StatBar.Text = "statusStrip1";
+            // 
+            // toolStripStatusPrg
+            // 
+            this.toolStripStatusPrg.Name = "toolStripStatusPrg";
+            this.toolStripStatusPrg.Size = new System.Drawing.Size(88, 21);
+            this.toolStripStatusPrg.Text = "State: Idle";
+            // 
+            // toolStripStatFileCount
+            // 
+            this.toolStripStatFileCount.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right)));
+            this.toolStripStatFileCount.Margin = new System.Windows.Forms.Padding(10, 5, 0, 3);
+            this.toolStripStatFileCount.Name = "toolStripStatFileCount";
+            this.toolStripStatFileCount.Size = new System.Drawing.Size(86, 29);
+            this.toolStripStatFileCount.Text = "TotalFiles";
+            this.toolStripStatFileCount.Visible = false;
+            // 
+            // toolStripStatTime
+            // 
+            this.toolStripStatTime.Margin = new System.Windows.Forms.Padding(5, 4, 5, 3);
+            this.toolStripStatTime.Name = "toolStripStatTime";
+            this.toolStripStatTime.Size = new System.Drawing.Size(117, 30);
+            this.toolStripStatTime.Text = "Elapsed time: ";
+            this.toolStripStatTime.Visible = false;
+            // 
+            // toolStripDuplicityFound
+            // 
+            this.toolStripDuplicityFound.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left;
+            this.toolStripDuplicityFound.Font = new System.Drawing.Font("Segoe UI", 8.883117F, System.Drawing.FontStyle.Bold);
+            this.toolStripDuplicityFound.Margin = new System.Windows.Forms.Padding(10, 5, 0, 3);
+            this.toolStripDuplicityFound.Name = "toolStripDuplicityFound";
+            this.toolStripDuplicityFound.Size = new System.Drawing.Size(169, 29);
+            this.toolStripDuplicityFound.Text = "Founded duplicity";
+            this.toolStripDuplicityFound.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1381, 835);
+            this.Controls.Add(this.StatBar);
+            this.Controls.Add(this.LblReult);
+            this.Controls.Add(this.TextBoxResult);
+            this.Controls.Add(this.ChkSsdDisk);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.BtnFolderSelect);
+            this.Controls.Add(this.SelectedFolderText);
+            this.Controls.Add(this.ChkInclSubdirs);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Find duplicity";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.StatBar.ResumeLayout(false);
+            this.StatBar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox ChkInclSubdirs;
+        private System.Windows.Forms.TextBox SelectedFolderText;
+        private System.Windows.Forms.Button BtnFolderSelect;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.FolderBrowserDialog SelectFolderDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox ChkSsdDisk;
+        private System.Windows.Forms.TextBox TextBoxResult;
+        private System.Windows.Forms.Label LblReult;
+        private System.Windows.Forms.StatusStrip StatBar;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusPrg;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatFileCount;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripDuplicityFound;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatTime;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+
+namespace Testapp
+{   
+    public struct FileInfos
+    {
+        public Int32 Index;             // index souboru
+        public string  FileName;        // nazaev souboru
+        public Int64 FileSize;          // velikost souboru
+        public bool Duplicity;
+
+    }
+
+    
+
+    public partial class Form1 : Form
+    {
+        public const int DiskTypeSSD = 16;
+        public const int DiskTypeHDD = 2;
+        public List<FileInfos> ProcFileList = new List<FileInfos>();
+        public Int32 FilesCount;
+        public CountdownEvent thrcounter = new CountdownEvent(1);
+        public Int32 DuplicityFilesCount = 0;
+
+        // porovnani obsahu svou souboru
+        private static bool CompareByReadFiles(string filename1, string filename2)
+        {
+            const Int32 BLOCK_SIZE = 65538;  
+            byte[] blockfile1 = new byte[BLOCK_SIZE];
+            byte[] blockfile2 = new byte[BLOCK_SIZE];
+            bool duplfinded = true;
+            try
+            {
+                using (FileStream stream1 = File.OpenRead(filename1))
+                using (FileStream stream2 = File.OpenRead(filename2))
+                    while (true)
+                    {
+
+                        int rdlen1 = stream1.Read(blockfile1, 0, BLOCK_SIZE);
+                        int rdlen2 = stream2.Read(blockfile2, 0, BLOCK_SIZE);
+                        if (!blockfile1.SequenceEqual(blockfile2))
+                        {
+                            duplfinded = false;
+                            break;
+                        }
+                        if (rdlen1 == 0 || rdlen2 == 0) break;
+                    }
+            }
+            catch (System.Exception excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+
+
+            return duplfinded;
+        }
+        
+
+
+        // vlakno pro porovnani obsahu dvou soboru
+        private void Comparefiles(Int32 Index)
+        {
+            Int16 addindex = 1;
+            var file1 = new FileInfosWrapper { onerec = ProcFileList[Index] };
+            var file2 = new FileInfosWrapper { onerec = ProcFileList[Index + addindex] };
+            thrcounter.AddCount();
+            do
+            {
+                file1.onerec.Duplicity = CompareByReadFiles(file1.onerec.FileName, file2.onerec.FileName);
+
+                if (file1.onerec.Duplicity)
+                {
+                    TextBoxResult.Invoke((MethodInvoker)delegate
+                    {
+                        TextBoxResult.AppendText("Duplicity files: " + file1.onerec.FileName + " and " + file2.onerec.FileName + "\r\n");
+                        DuplicityFilesCount++;
+                    });
+                    Application.DoEvents();
+                }
+                addindex++;
+                if ((Index+addindex)==FilesCount) {
+                    break;
+                }
+                file2.onerec = ProcFileList[Index + addindex];
+            } while (file1.onerec.FileSize == file2.onerec.FileSize);
+            thrcounter.Signal();
+            
+        }
+
+        // zjisteni poctu souboru, jecjich velikosti a vytvoreni seznamu souboru, ktere maji byt porovnany       
+        public void GetFilesInfo(string Path, bool InclSubDirs, List<FileInfos> FilesInfoList)
+        {
+            FileInfos CurFileInfo = new FileInfos();
+            Int32 findex = 0;
+            try
+            {
+                if (InclSubDirs)
+                {                  
+                    foreach (string curfile in System.IO.Directory.GetFiles(Path, "*", SearchOption.AllDirectories))
+                    {
+                        CurFileInfo.FileSize = new FileInfo(curfile).Length;
+                        CurFileInfo.FileName = curfile;
+                        CurFileInfo.Index = findex;
+                        CurFileInfo.Duplicity = false;
+                        FilesInfoList.Add(CurFileInfo);
+                        findex++;
+                    }
+
+                } else
+                {
+                    foreach (string curfile in System.IO.Directory.GetFiles(Path, "*"))
+                    {
+                        CurFileInfo.FileSize = new FileInfo(curfile).Length;
+                        CurFileInfo.FileName = curfile;                       
+                        CurFileInfo.Index = findex;
+                        CurFileInfo.Duplicity = false;
+                        FilesInfoList.Add(CurFileInfo);
+                        findex++;
+                    }
+                }
+                
+            }
+            catch (System.Exception excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // vyber adresare pro hledani duplicitnich souboru
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (SelectFolderDialog.ShowDialog() == DialogResult.OK)
+            {
+                SelectedFolderText.Text = SelectFolderDialog.SelectedPath;
+            }
+        }
+
+
+        // spusteni procesu vyhledavani duplicit
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Int32 ActIndex = 0;
+            bool InclSubDirs;
+            String PathFolder = SelectedFolderText.Text;
+            int outhtr, outaastthr, outthr1, outaastthr1,actthr;
+            int maxthrcount = 0;
+            Stopwatch FindProcTimer = new Stopwatch();
+            TimeSpan ElapsedTime;
+            string TotTimeStr;
+            toolStripStatFileCount.Visible = false;
+            toolStripDuplicityFound.Visible = false;
+            toolStripStatTime.Visible = false;
+            toolStripStatusPrg.Text = "Status: Counting Files";
+            TextBoxResult.Clear();
+            TextBoxResult.Refresh();
+            StatBar.Refresh();
+            FindProcTimer.Start();
+            if (!System.IO.Directory.Exists(PathFolder))
+            {
+                MessageBox.Show("Folder does not exists", "Folder error", MessageBoxButtons.OK);
+                return;
+            }
+            if (ChkInclSubdirs.Checked)
+            {
+                FilesCount = System.IO.Directory.GetFiles(PathFolder, "*", SearchOption.AllDirectories).Count();
+                InclSubDirs = true;
+
+            } else
+            {
+                FilesCount = System.IO.Directory.EnumerateFiles(PathFolder).Count();
+                InclSubDirs = false;
+            }
+            
+            string totalfiles = FilesCount.ToString();           
+            toolStripStatFileCount.Visible = true;
+            toolStripStatFileCount.Text = "Total files: " + totalfiles;
+            toolStripStatusPrg.Text = "Status: Listing files";
+            StatBar.Refresh();
+            
+            if (ProcFileList.Count>0)
+            {
+                ProcFileList.Clear();
+            }            
+            GetFilesInfo(PathFolder, InclSubDirs,ProcFileList);
+
+            ProcFileList.Sort((s1,s2) => s1.FileSize.CompareTo(s2.FileSize));     // serazeni souboru dle velikosti souboru
+            if (ChkSsdDisk.Checked)                                               // maximalni pocet vlaken pro dle typu disku> HDD/SSD
+            {
+                ThreadPool.SetMinThreads(1, 1);
+                ThreadPool.SetMaxThreads(DiskTypeSSD, DiskTypeSSD);
+                maxthrcount = DiskTypeSSD;
+            } else
+            {
+                ThreadPool.SetMinThreads(1, 1);
+                ThreadPool.SetMaxThreads(DiskTypeHDD, DiskTypeHDD);
+                maxthrcount = DiskTypeHDD;
+            }
+            toolStripStatTime.Visible = true;
+            DuplicityFilesCount = 0;
+            toolStripStatusPrg.Text = "Status: Processing";
+            TextBoxResult.AppendText("******** Duplicity Files ********\r\n");
+
+            // porovnavani souboru dle velikosti
+            do
+            {
+                if (ProcFileList[ActIndex].FileSize == ProcFileList[ActIndex + 1].FileSize)
+                {
+                    ThreadPool.QueueUserWorkItem(Index => Comparefiles((Int32)Index), ActIndex);       // vlakno pro porovnani
+
+                    ThreadPool.GetAvailableThreads(out outhtr, out outaastthr);
+                    ThreadPool.GetMaxThreads(out outthr1, out outaastthr1);
+                    actthr = maxthrcount - outhtr;
+                    StatBar.Refresh();
+                    ActIndex++;
+                }
+                else
+                {
+                    ActIndex++;
+                }
+                if ((ActIndex % 1000) == 0 && (FilesCount>8000)) {
+                    ElapsedTime = FindProcTimer.Elapsed;
+                    TotTimeStr = String.Format("{0:00}:{1:00}:{2:00}", ElapsedTime.Hours, ElapsedTime.Minutes, ElapsedTime.Seconds);
+                    toolStripStatTime.Text = "Elapsed time: " + TotTimeStr;
+                    StatBar.Refresh();
+                }
+
+            } while (ActIndex < FilesCount-1);
+            do                                          // cekani na dokonceni vsech threadu
+            {
+                Application.DoEvents();
+                Thread.Sleep(200);
+                ElapsedTime = FindProcTimer.Elapsed;
+                TotTimeStr = String.Format("{0:00}:{1:00}:{2:00}", ElapsedTime.Hours, ElapsedTime.Minutes, ElapsedTime.Seconds);
+                toolStripStatTime.Text = "Elapsed time: " + TotTimeStr;
+                StatBar.Refresh();
+                
+            } while (thrcounter.CurrentCount > 1);
+
+            FindProcTimer.Stop();
+
+            toolStripStatusPrg.Text = "State: Idle";
+            toolStripStatFileCount.Visible = false;
+            toolStripStatTime.Visible = false;
+            toolStripDuplicityFound.Text = "Duplicity files count: " + DuplicityFilesCount.ToString();
+            toolStripDuplicityFound.Visible = true;
+            StatBar.Refresh();
+            ElapsedTime = FindProcTimer.Elapsed;
+            TotTimeStr = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ElapsedTime.Hours, ElapsedTime.Minutes, ElapsedTime.Seconds, ElapsedTime.Milliseconds / 10);
+            MessageBox.Show("Finished\r\n" + "Total time:" + TotTimeStr, "Process Finished", MessageBoxButtons.OK);
+        }
+    }
+
+
+    public class FileInfosWrapper
+    {
+        public FileInfos onerec;
+    }
+}
